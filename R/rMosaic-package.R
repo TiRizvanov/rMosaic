8372d65cf8a0096b4e039b41d27d2d4adb88ceb1
Timur Rizvanov <1812timurr@gmail.com>
2026-05-24 17:17:47 -0400

    docs: add Dries Lab and BU UROP funding acknowledgements
    - DESCRIPTION: add Dries Lab and Boston University as funders
      (role = "fnd") in Authors@R alongside the existing authors.
    - R/rMosaic-package.R: add @section Acknowledgements roxygen block
      noting the Dries Lab as host laboratory and BU UROP as funding source.
    - README.md: add an "Acknowledgements" section linking to drieslab.com.
    - Regenerate man/rMosaic-package.Rd; trivial whitespace touch in
      man/mosaic.Rd from roxygenise.

--- R/rMosaic-package.R
+++ R/rMosaic-package.R
@@ -1,4 +1,8 @@
 #' @keywords internal
+#' @section Acknowledgements:
+#' Developed in the Dries Lab at Boston University. This work was supported by
+#' the Dries Lab and the Boston University Undergraduate Research Opportunities
+#' Program (UROP).
 "_PACKAGE"
 
 ## usethis namespace: start
